15096cbc370b0b5d959087c53508e038fd6c53f0
BORANBAYEV <105725315+BORANBAYEV@users.noreply.github.com>
2022-06-01 23:44:57 +0600

    Update json,example.py

--- json,example.py
+++ json,example.py
@@ -1,12 +1,17 @@
 import json
 import requests
 
-# url = "https://jsonplaceholder.typicode.com/posts"
-# url = "https://api.instantwebtools.net/v1/airlines"
-# url = "https://api.instantwebtools.net/v1/passenger/:id2"
-url = "https://jsonplaceholder.typicode.com/posts/1"
+# url = "https://jsonplaceholder.typicode.com/posts/1"
+# response = requests.get(url)
+# json_data = response.content.decode()
+# print(json_data)
+# with open('get_one.json', 'w') as file:
+#     json.dump(get_one.json, file)
+
+url = "https://api.instantwebtools.net/v1/airlines"
 response = requests.get(url)
 json_data = response.content.decode()
 print(json_data)
-with open('get_one.json', 'w') as file:
-    json.dump(get_one.json, file)
+for i in range(0,10):
+    with open(f'{i}get_one.json', 'w') as file:
+        json.dump(json_data, file)
